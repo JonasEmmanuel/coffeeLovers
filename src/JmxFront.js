751fa26da13e38a52a5b76981a59ad0f395950c7
jhounsou <FrINS@1718>
2018-12-21 17:58:55 +0100

    update311218

--- src/JmxFront.js
+++ src/JmxFront.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import NavBar from './NavBar'
-import axios from 'axios'
+import React from "react";
+import NavBar from "./NavBar";
+import axios from "axios";
 import {
   Input,
   InputGroup,
@@ -9,60 +9,51 @@ import {
   Label,
   Form,
   ButtonGroup
-} from 'reactstrap'
-import './customCss.css'
-import Select from 'react-select'
+} from "reactstrap";
+import "./customCss.css";
+import Select from "react-select";
 
 const options = [
-  { value: 'GET', label: 'GET' },
-  { value: 'POST', label: 'POST' }
-]
+  { value: "GET", label: "GET" },
+  { value: "POST", label: "POST" }
+];
+
+const ShowServices = props => {
+  return (
+    <div>
+      <div>{props.serviceId}</div>
+    </div>
+  );
+};
 
 const OperationInfo = props => {
   return (
-    <div className='card'>
-      <h5 className='card-header'>{props.operationDesc}</h5>
-      <div className='card-body'>
-        {console.log('iriririr')}
-        {props.myparams.map((idParams, i) => {
-          return (
-            <Col sm={10}>
-              <Label>
-                <b>
-                  {props.allparams[idParams].name}(
-                  {props.allparams[idParams].type}):
-                </b>
-              </Label>
-              <Input
-                type='text'
-                value={props.allparams[idParams].value}
-                onChange={event => props.onChangeParamsValue(event, idParams)}
-              />
-            </Col>
-          )
-        })}
+    <div className="card">
+      <h5 className="card-header">{props.operationDesc}</h5>
+      <div className="card-body">
+        {}
         <Col sm={10}>
           <br />
           <br />
           <Label>Resultat:</Label>
-          <Input type='textarea' id='paste' value={props.result} />
+          <Input type="textarea" id="paste" value={props.result} />
         </Col>
       </div>
-      <div className='card-footer'>
+      <div className="card-footer">
         <ButtonGroup>
-          <Button outline color='success'>
+          <Button outline color="success">
             Executer Requete
           </Button>
-          <div id='right'>
+          <div id="right">
             <Button
               outline
-              color='danger'
+              color="danger"
               onClick={() => {
                 props.onReset(
                   props.myparams,
                   props.allparams,
                   props.operationId
-                )
+                );
               }}
             >
               Reset Params
@@ -71,49 +62,49 @@ const OperationInfo = props => {
         </ButtonGroup>
       </div>
     </div>
-  )
-}
+  );
+};
 
 class JmxFront extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       services: [],
       params: {},
       operations: {}
-    }
+    };
   }
 
-  componentDidMount () {
-    axios.get(` http://www.mocky.io/v2/5c0d77122f00002900e2e584`).then(res => {
-      const myDonnees = res.data.value
+  componentDidMount() {
+    axios.get(`http://www.mocky.io/v2/5c0d77122f00002900e2e584`).then(res => {
+      const myDonnees = res.data.value;
       // console.log(myDonnees)
-      var serviceName = ''
-      var operationsName = ''
-      var descOperation = ''
-      var returnOperation = ''
-      var argsName
+      var serviceName = "";
+      var operationsName = "";
+      var descOperation = "";
+      var returnOperation = "";
+      var argsName;
       // const md = JSON.parse(res);
-      var value2 = ''
+      var value2 = "";
       Object.keys(myDonnees).forEach(operations => {
-        var value = myDonnees[operations]
+        var value = myDonnees[operations];
         // getServiceName
-        serviceName = operations // type=ShrimpMaintenance
+        serviceName = operations; // type=ShrimpMaintenance
         // console.log(operations)
-        var argsCollected = value.op // all operation : reloadConfig, shutdownSystem
+        var argsCollected = value.op; // all operation : reloadConfig, shutdownSystem
         // get allOperations for this Service
         // console.log(argsCollected)
-        var tabOps = []
+        var tabOps = [];
         Object.keys(argsCollected).forEach(arg => {
           // console.log(arg)
           // operationsName = arg
-          value2 = argsCollected[arg] // Table of args for this operations
+          value2 = argsCollected[arg]; // Table of args for this operations
           // console.log(value2.args)
-          returnOperation = value2.ret
-          descOperation = value2.desc
-          argsName = value2.args
+          returnOperation = value2.ret;
+          descOperation = value2.desc;
+          argsName = value2.args;
           // getArgs for this operations
-          var tapParams = []
+          var tapParams = [];
           argsName.map((a, i) => {
             // console.log(a.name)
             this.setState(prevState => {
@@ -123,21 +114,21 @@ class JmxFront extends React.Component {
                   .substring(2, 15) +
                 Math.random()
                   .toString(36)
-                  .substring(2, 15)
-              console.log(y)
+                  .substring(2, 15);
+              console.log(y);
 
               // Ajouter un element cle valeur à la liste des parametres.
               var p = {
                 ...prevState.params,
-                [y]: { name: a.name, type: a.type, desc: a.desc, value: '' }
-              }
-              tapParams.push(y)
+                [y]: { name: a.name, type: a.type, desc: a.desc, value: "" }
+              };
+              tapParams.push(y);
               // console.log(tapParams)
 
               // clear Array
-              return { params: p }
-            })
-          })
+              return { params: p };
+            });
+          });
           this.setState(prevState => {
             var y2 =
               Math.random()
@@ -145,8 +136,8 @@ class JmxFront extends React.Component {
                 .substring(2, 15) +
               Math.random()
                 .toString(36)
-                .substring(2, 15)
-            tabOps.push(y2)
+                .substring(2, 15);
+            tabOps.push(y2);
             var p2 = {
               ...prevState.operations,
               [y2]: {
@@ -154,35 +145,35 @@ class JmxFront extends React.Component {
                 desc: descOperation,
                 ret: returnOperation,
                 paramsId: tapParams,
-                result: ''
+                result: ""
               }
-            }
-            return { operations: p2 }
-          })
-        })
+            };
+            return { operations: p2 };
+          });
+        });
         this.setState(prevState => {
           var serv = [
             ...prevState.services,
             { name: operations, allOps: tabOps }
-          ]
-          return { services: serv }
-        })
-      })
-    })
+          ];
+          return { services: serv };
+        });
+      });
+    });
   }
 
-  onCall = endpoint => {}
+  onCall = endpoint => {};
 
   onChangeParamsValue = (event, pa) => {
-    const valueX = event.target.value
+    const valueX = event.target.value;
     this.setState(prevState => {
-      const p = { ...prevState.params }
-      p[pa].value = valueX
+      const p = { ...prevState.params };
+      p[pa].value = valueX;
       // console.log(indexParams)
       // p[indexParams].value = valueX
-      return { params: p }
-    })
-  }
+      return { params: p };
+    });
+  };
 
   /* onChangeSelect = (v, index) => {
     this.setState(prevState => {
@@ -207,29 +198,45 @@ class JmxFront extends React.Component {
     })
   } */
 
-  render () {
-    {
-      return (
-        <div>
-          {Object.keys(this.state.operations).forEach(opId => {
-            var op = this.state.operations[opId]
-            // console.log(op.desc)
-            
-              <div className='container-fluid'>
-                <div className='row'>
-                  <div className='col-md-2' />
-                  <div className='col-md-8' id='cardBoard'>
-                    toto
-                  </div>
-                  <div className='col-md-2' />
-                </div>
-              </div>
-            
-          })}
-        </div>
-      )
-    }
+  render() {
+    return <div>toto</div>;
   }
 }
 
-export default JmxFront
+class DisplayArgs extends React.Component {
+  render() {
+    const args = this.props.args;
+    return (
+      <div>
+        <div>
+          {Object.keys(args).forEach(id => {
+            // si id se retrouve dans le tableau localArgs alors afficher argument
+            props.localArgs.find(argsCurrent => {
+              return argsCurrent === id;
+            });
+          })}
+          <Label>{args.name}</Label>
+          <Input type="text" value={args.value} />
+          <br />
+          <i>{args.desc}</i>: <i>{args.type}</i>
+        </div>
+      </div>
+    );
+  }
+}
+
+class DisplayOperations extends React.Component {
+  render() {
+    const operations = props.operations;
+    <div>
+      <div>
+        <Label>{operations.name}</Label>
+        <b>{operations.desc}</b>
+        <Input type="textarea" value={operations.result} />
+        <DisplayArgs localArgs={operations.paramsId} args={props.params} />
+      </div>
+    </div>;
+  }
+}
+
+export default JmxFront;
--- src/ReqJolokia.js
+++ src/ReqJolokia.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import randomstring from 'randomstring'
-import axios from 'axios'
-import initStateFromJolokia from './Utils'
+import React from "react";
+import randomstring from "randomstring";
+import axios from "axios";
+import initStateFromJolokia from "./Utils";
 
 class ReqJolokia extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       services: [],
       params: {},
       operations: {}
-    }
+    };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     axios.get(` http://www.mocky.io/v2/5c0d77122f00002900e2e584`).then(res => {
-      const myDonnees = res.data.value
+      const myDonnees = res.data.value;
       // console.log(myDonnees)
-      var serviceName = ''
-      var operationsName = ''
-      var descOperation = ''
-      var returnOperation = ''
-      var argsName
+      var serviceName = "";
+      var operationsName = "";
+      var descOperation = "";
+      var returnOperation = "";
+      var argsName;
       // const md = JSON.parse(res);
-      var value2 = ''
+      var value2 = "";
       Object.keys(myDonnees).forEach(operations => {
-        var value = myDonnees[operations]
+        var value = myDonnees[operations];
         // getServiceName
-        serviceName = operations // type=ShrimpMaintenance
+        serviceName = operations; // type=ShrimpMaintenance
         // console.log(operations)
-        var argsCollected = value.op // all operation : reloadConfig, shutdownSystem
+        var argsCollected = value.op; // all operation : reloadConfig, shutdownSystem
         // get allOperations for this Service
         // console.log(argsCollected)
-        var tabOps = []
+        var tabOps = [];
         Object.keys(argsCollected).forEach(arg => {
           // console.log(arg)
           // operationsName = arg
-          value2 = argsCollected[arg] // Table of args for this operations
+          value2 = argsCollected[arg]; // Table of args for this operations
           // console.log(value2.args)
-          returnOperation = value2.ret
-          descOperation = value2.desc
-          argsName = value2.args
+          returnOperation = value2.ret;
+          descOperation = value2.desc;
+          argsName = value2.args;
           // getArgs for this operations
-          var tapParams = []
+          var tapParams = [];
           argsName.map((a, i) => {
             // console.log(a.name)
             this.setState(prevState => {
@@ -52,21 +52,21 @@ class ReqJolokia extends React.Component {
                   .substring(2, 15) +
                 Math.random()
                   .toString(36)
-                  .substring(2, 15)
-              console.log(y)
+                  .substring(2, 15);
+              console.log(y);
 
               // Ajouter un element cle valeur à la liste des parametres.
               var p = {
                 ...prevState.params,
-                [y]: { name: a.name, type: a.type, desc: a.desc, value: '' }
-              }
-              tapParams.push(y)
+                [y]: { name: a.name, type: a.type, desc: a.desc, value: "" }
+              };
+              tapParams.push(y);
               // console.log(tapParams)
 
               // clear Array
-              return { params: p }
-            })
-          })
+              return { params: p };
+            });
+          });
           this.setState(prevState => {
             var y2 =
               Math.random()
@@ -74,8 +74,8 @@ class ReqJolokia extends React.Component {
                 .substring(2, 15) +
               Math.random()
                 .toString(36)
-                .substring(2, 15)
-            tabOps.push(y2)
+                .substring(2, 15);
+            tabOps.push(y2);
             var p2 = {
               ...prevState.operations,
               [y2]: {
@@ -83,26 +83,37 @@ class ReqJolokia extends React.Component {
                 desc: descOperation,
                 ret: returnOperation,
                 paramsId: tapParams,
-                result: ''
+                result: ""
               }
-            }
-            return { operations: p2 }
-          })
-        })
+            };
+            return { operations: p2 };
+          });
+        });
         this.setState(prevState => {
           var serv = [
             ...prevState.services,
             { name: operations, allOps: tabOps }
-          ]
-          return { services: serv }
-        })
-      })
-    })
+          ];
+          return { services: serv };
+        });
+      });
+    });
   }
 
-  render () {
-    return <div>toto</div>
+  render() {
+    return <div>toto</div>;
   }
 }
 
-export default ReqJolokia
+class showArgs extends React.Component {
+  render() {
+    const args = this.props.args;
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default ReqJolokia;
